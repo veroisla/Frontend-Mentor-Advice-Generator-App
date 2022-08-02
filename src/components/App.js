b475172b46444e9d4f9f912bb2b83666d5df9f15
Veronica isla <veroislagleaniz@gmail.com>
2022-08-02 12:24:39 +0200

    created structure, files of variables and reset and added favicon

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState } from 'react';
+//
+import '../styles/reset.scss';
+import '../styles/vars.scss';
+
+function App() {
+  return <div>Hola mundo</div>;
+}
+
+export default App;
